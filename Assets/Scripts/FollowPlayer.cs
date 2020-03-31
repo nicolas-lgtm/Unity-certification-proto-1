fac6e46f039217b77eac1a14cadb9618ffa2ec72
Ernouf <48248463+Ernouf@users.noreply.github.com>
2020-03-31 09:21:03 +0200

    Initial Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15f;
+    private float horizontalInput;
+    private float turnSpeed = 100f;
+    private float verticalInput;
+    private float acceleration;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            acceleration = 1.5f;
+        }
+        else
+        {
+            acceleration = 1;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput * acceleration);
+
+        verticalInput = Input.GetAxis("Vertical");
+        if(verticalInput != 0) transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
